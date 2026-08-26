0602b08e8be81615c3776c0163da6f00e39af918
stw1zze <stw1zze@gmail.com>
2026-08-26 17:03:57 +0200

    Add button click event to display message

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("myButton");
+let message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+        message.textContent = "🔥 Вау ебать ты лох!";
+});
